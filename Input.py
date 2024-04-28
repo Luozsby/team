35387f4f1e2a73019c068c70ebd261de26c02d2d
luo <zsbyu5201314@gmail.com>
2024-04-28 22:38:31 +0800

    第三次提交

---
+++ Input.py
@@ -0,0 +1,5 @@
+def Input():
+    print('请输入2个整数(用空格分割):',end='')
+    a,b=map(int,input().split())
+    return ([a,b])
+
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-123
+import Input
+import Output
+def main():
+    s=max(Input.Input())
+    Output.Output(s)
+if __name__=='__main__':
+    main()
